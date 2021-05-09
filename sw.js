4f43132745a3f3de2ef3ed49515ee8876fe07082
agampreetsingh <agampreet_singh@yahoo.com>
2021-05-09 19:45:15 +0530

    added support for PWA

--- sw.js
+++ sw.js
@@ -2,23 +2,9 @@ console.log('Started', self);
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('airhorner').then(function(cache) {
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
-        'index.html',
-        'css/styles.css',
-        'js/a2hs/pwainstaller.js',
-        'js/constants.js',
-        'js/scraper.js',
-        'js/util.js',
-        'js/themechanger.js',
-        'js/vaccineDataUtil.js',
-        'images/touch/homescreen48.png',
-        'images/touch/homescreen72.png',
-        'images/touch/homescreen96.png',
-        'images/touch/homescreen144.png',
-        'images/touch/homescreen168.png',
-        'images/touch/homescreen192.png',
-        'images/touch/homescreen512.png'
+        'index.html'
       ]);
     })
   );
